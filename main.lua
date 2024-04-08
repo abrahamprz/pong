a79e478fc02f9121ecb41bbf9f88266948ac8a1a
Abraham <fcoabrahamprz@gmail.com>
2024-04-08 14:10:10 -0700

    The Resize Update

--- main.lua
+++ main.lua
@@ -2,8 +2,8 @@
     GD50 2018
     Pong Remake
 
-    pong-11
-    "The Audio Update"
+    pong-12
+    "The Resize Update"
 
     -- Main Program --
 
@@ -88,7 +88,7 @@ function love.load()
     -- initialize window with virtual resolution
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
-        resizable = false,
+        resizable = true,
         vsync = true
     })
 
@@ -113,6 +113,14 @@ function love.load()
     -- (used for beginning, menus, main game, high score list, etc.)
     -- we will use this to determine behavior during render and update
     gameState = 'start'
+end
+
+--[[
+    Called by LÖVE whenever we resize the screen; here, we just want to pass in the
+    width and height to push so our virtual resolution can be resized as needed.
+]]
+function love.resize(w, h)
+    push:resize(w, h)
 end
 
 --[[
